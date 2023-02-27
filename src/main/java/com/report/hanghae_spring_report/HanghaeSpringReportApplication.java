a74156e6811f09f5b15875f67cda31ba7eb5b7ed
jangjinhyeok <jangjh45@naver.com>
2023-02-27 09:37:17 +0900

    Initial commit

---
+++ src/main/java/com/report/hanghae_spring_report/HanghaeSpringReportApplication.java
@@ -0,0 +1,13 @@
+package com.report.hanghae_spring_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpringReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpringReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/report/hanghae_spring_report/HanghaeSpringReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report.hanghae_spring_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeSpringReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b4228eb4fddc1513e466ff1ca69d6c980238a3d0
jangjinhyeok <jangjh45@naver.com>
2023-03-16 02:50:14 +0900

    refactor: 예외처리 수정

--- src/main/java/com/report/hanghae_spring_report/common/ApiExceptionController.java
+++ src/main/java/com/report/hanghae_spring_report/common/ApiExceptionController.java
@@ -12,16 +12,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice // 예외 처리를 담당하는 클래스에 적용
 public class ApiExceptionController {
 
+//    @ExceptionHandler(value = {ApiException.class})
+//    protected ResponseEntity<ApiExceptionResponse> handleCustomException(ApiException e) {
+//        log.error("handleCustomException : {}", e.getErrorCode());
+//        return ApiExceptionResponse.toResponseEntity(e.getErrorCode());
+//    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {ApiException.class})
-    protected ResponseEntity<ApiExceptionResponse> handleCustomException(ApiException e) {
+    protected ResponseEntity handleCustomException(ApiException e) {
         log.error("handleCustomException : {}", e.getErrorCode());
-        return ApiExceptionResponse.toResponseEntity(e.getErrorCode());
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(new ErrorResult(
+                e.getErrorCode().getStatus().value(),
+                e.getErrorCode().getStatus(),
+                e.getErrorCode().getMessage()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ErrorResult singupExHandler(MethodArgumentNotValidException e) {
         log.error("ERROR : {}", e.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
-        return new ErrorResult(HttpStatus.BAD_REQUEST, e.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
+        return new ErrorResult(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST,
+                e.getBindingResult()
+                        .getFieldErrors().get(0)
+                        .getDefaultMessage());
     }
 }
--- src/main/java/com/report/hanghae_spring_report/common/ErrorResult.java
+++ src/main/java/com/report/hanghae_spring_report/common/ErrorResult.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Data
 @AllArgsConstructor
 public class ErrorResult {
+    private int status;
     private HttpStatus code;
     private String message;
 }
